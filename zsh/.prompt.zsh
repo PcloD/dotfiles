e36160c63ea8c2856c4bc8031455938fc353c1f3
Keith Peters <kp@bit-101.com>
2018-03-03 07:03:34 -0500

    prompt

--- zsh/.prompt.zsh
+++ zsh/.prompt.zsh
@@ -5,14 +5,16 @@ ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗ %{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=" ✔ "
 
 function zle-line-init zle-keymap-select {
-  VIM_MODE="%{$fg[yellow]%}[NORMAL]%{$reset_color%}"
-  PATH_PROMPT="%{$fg[white]%}[%~]%{$reset_color%}"
+  NORMAL_MODE="%{$fg[yellow]%}[VIM]%{$reset_color%}"
+  INSERT_MODE="%{$fg[blue]%}[ZSH]%{$reset_color%}"
+  MODE_PROMPT="${${KEYMAP/vicmd/$NORMAL_MODE}/(main|viins)/$INSERT_MODE}"
+  PATH_PROMPT="%{$fg[green]%}[%~]%{$reset_color%}"
   GIT_PROMPT=$(git_prompt_info)
-  COMMAND_PROMPT="%{$fg[yellow]%}%#%{$reset_color%} "
+  COMMAND_PROMPT="%{$fg[blue]%}%#%{$reset_color%} "
 
 # put it all together
-  PROMPT=$'$PATH_PROMPT$GIT_PROMPT$COMMAND_PROMPT'
-  RPS1='${${KEYMAP/vicmd/$VIM_MODE}/(main|viins)/}'
+  PROMPT=$'$PATH_PROMPT$COMMAND_PROMPT'
+  RPS1=$'$GIT_PROMPT$MODE_PROMPT'
   zle reset-prompt
 }
 zle -N zle-line-init
