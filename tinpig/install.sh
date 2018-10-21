b05eb68db9ebb08abd64b334f3e0a8398d7bf5e0
Keith Peters <kp@bit-101.com>
2018-10-21 10:21:07 -0400

    installer for tinpig

---
+++ tinpig/install.sh
@@ -0,0 +1,2 @@
+mv ~/.config/tinpig/templates ~/.config/tinpig/templates.bak
+sudo ln -s ~/dotfiles/tinpig/templates ~/.config/tinpig/templates
---
+++ tinpig/readme.md
@@ -0,0 +1,3 @@
+1. install tinpig `sudo npm install -g tinpig`
+2. run once to set up config dir
+3. run `install.sh` to symlink templates.
