11c019a5962c2ec52b60f58d0e9f413eca42bf95
Keith Peters <kp@bit-101.com>
2018-10-27 18:18:35 -0400

    urxvt configs

---
+++ urxvt/install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo ln -s ~/dotfiles/urxvt/.Xdefaults ~/.Xdefaults
+sudo ln -s ~/dotfiles/urxvt/.urxvt ~/.urxvt
---
+++ urxvt/readme.md
@@ -0,0 +1,2 @@
+1. apt install wmctrl
+2. run `install.sh` to create symlinks
